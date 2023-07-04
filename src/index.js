8807d0e60735755dfe6e3ecb48476b6325f80c0a
Chiragsinghal24 <chiragsinghal2406@gmail.com>
2023-07-04 23:39:38 +0530

    Added Login and other pages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,74 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Auth from './components/Auth.jsx';
+import LoginForm from './components/Login.jsx';
+import RegisterForm from './components/Register.jsx';
+import Phonenoform from './components/Phoneno.jsx';
+import Numberverificationform from './components/Numberverification.jsx'
+import Domainselectionform from './components/Selectdomain.jsx'
+import Skillselectionform from './components/Otherskills';
+import TermsForm from './components/T&c';
+import Birthdayform from './components/Birthday';
+import Thanks from './components/Thanks';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      {
+        index: true,
+        element: <LoginForm />
+      },
+      {
+        path: "register",
+        element: <RegisterForm />
+      },
+      {
+        path: "phone",
+        element: <Phonenoform />
+      },
+      {
+        path: "verification",
+        element: <Numberverificationform />
+      },
+      {
+        path: "domain",
+        element: <Domainselectionform />
+      },
+      {
+        path: "terms",
+        element: <TermsForm />
+      },
+      {
+        path: "skills",
+        element: <Skillselectionform />
+      },
+      {
+        path: "birthday",
+        element: <Birthdayform />
+      }
+    ]
+  },
+  {
+    path: "/auth/thanks",
+    element: <Thanks />
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

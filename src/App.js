ec51757d0f73a52b2b83a164c99dcd363526f550
PHANIGONUGUNTA <84675623+phanigonugunta@users.noreply.github.com>
2023-07-04 16:36:19 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import "./App.css";
+import './App.css';
+import Nav from './components/HomePage/Navbar'
+import Flixlogo from "./flix_Logo.svg"
+import Heading from './components/HomePage/Heading';
+import Section from './components/HomePage/Section';
+import Post from './components/HomePage/Post'
+import PostCard from './components/HomePage/PostCard';
+import ProfileList from './components/HomePage/ProfileList';
+import Profile from './components/HomePage/Profile'
 
 function App() {
-	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
-	);
+  return (
+    <>
+    <div className="App flex flex-row">
+      <div class="basis-1/4 bg-yellow-200 rounded-r-3xl my-2">
+        <Nav props1 = {Flixlogo}></Nav>
+      </div>
+      <div class="basis-1/2 pr-3">
+        <Heading></Heading>
+        <Section></Section>
+        <Post></Post>
+        <PostCard></PostCard>
+      </div>
+      <div class="basis-1/4">
+        <Profile></Profile>
+        <ProfileList></ProfileList>
+      </div>
+    </div>
+    </>
+  );
 }
 
 export default App;

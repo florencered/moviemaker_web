069a0f722bb05339a03df6c1287e3879e01a4f4b
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-07-05 18:48:30 +0530

    added images in top cities

--- src/components/SearchCity/TopCities.jsx
+++ src/components/SearchCity/TopCities.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import { TopCitiesCard } from "./TopCitiesCard";
 
 export const TopCities = () => {
+	const citiesJSON = [
+		{ id: 1, city: "Bangalore", src: "/images/cities/bangalore.svg" },
+		{ id: 2, city: "Hyderabad", src: "images/cities/hyderabad.svg" },
+		{ id: 3, city: "Mumbai", src: "images/cities/mumbai.svg" },
+		{ id: 4, city: "Chennai", src: "images/cities/chennai.svg" },
+		{ id: 5, city: "Delhi-NCR", src: "images/cities/delhi.svg" },
+		{ id: 6, city: "Chenadigarh", src: "images/cities/chandigarh.svg" },
+		{ id: 8, city: "Ahmedabad", src: "images/cities/ahmedabad.svg" },
+		{ id: 8, city: "Kolkata", src: "images/cities/kolkata.svg" },
+		{ id: 9, city: "Srinagar", src: "images/cities/srinagar.svg" },
+		{ id: 10, city: "Goa", src: "images/cities/goa.svg" },
+		{ id: 11, city: "Pune", src: "images/cities/pune.svg" },
+		{ id: 12, city: "Kochi", src: "images/cities/kochi.svg" },
+	];
+
 	return (
 		<div className="bg-[#F3E59D] rounded-lg flex flex-col items-center justify-center">
 			<div className="w-[40vw] px-8 py-4 pb-8">
 				<h1 className="font-bold mb-8">Top Cities</h1>
 				<div className="grid grid-cols-4 gap-x-4 gap-y-8">
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Mumbai" />
-					<TopCitiesCard city="Kochi" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
-					<TopCitiesCard city="Bangalore" />
+					{citiesJSON.map((city) => (
+						<div key={city.id}>
+							<TopCitiesCard city={city.city} src={city.src} />
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
--- src/components/SearchCity/TopCitiesCard.jsx
+++ src/components/SearchCity/TopCitiesCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export const TopCitiesCard = ({ city }) => {
+export const TopCitiesCard = ({ city, src }) => {
 	return (
 		<div>
 			<div className="flex flex-col items-center justify-between gap-4">
 				<img
 					className="w-[75px] rounded-[50%]"
-					src="https://static.amazon.jobs/locations/23/thumbnails/bangalore-thumb1.jpg?1630709762"
+					src={src}
 					alt="city-img"
 				/>
 				<h3 className=" -mt-2">{city}</h3>

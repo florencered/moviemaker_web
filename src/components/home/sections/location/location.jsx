a3603f62cb9f7c765d487cc445b3ced1b626a5ac
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-07-05 22:55:59 +0530

    added reactive ui after list click

--- src/components/home/sections/location/location.jsx
+++ src/components/home/sections/location/location.jsx
@@ -5,7 +5,7 @@ export const Location = () => {
 	const [monsters, setMonsters] = useState([]);
 	const [filteredMonsters, setFilteredMonsters] = useState([]);
 	const [drop, setDrop] = useState(false);
-	const [html, setHtml] = useState("");
+	const [html, setHtml] = useState("Select City");
 
 	useEffect(() => {
 		fetch("https://countriesnow.space/api/v0.1/countries/population/cities")
@@ -30,33 +30,57 @@ export const Location = () => {
 	};
 
 	const listClick = (e) => {
-		console.log("hello");
-		setHtml(e.target.innnerHTML);
+		console.log(e.target.innerHTML);
+		setHtml(e.target.innerHTML);
+		setDrop(false);
+	};
+
+	const unFocus = () => {
+		setTimeout(() => {
+			if (drop) setDrop(false);
+		}, 300);
 	};
 
 	return (
 		<div className="flex flex-col items-center justify-center gap-8 mt-[10vh]">
 			<div className="w-[40vw] flex flex-col bg-[#F3E59D] items-center justify-center  rounded-md">
-				<div className=" px-4 py-2  w-[40vw]">
-					<input
-						id="city-search"
-						className="shadow-none border-none bg-transparent focus:outline-none w-full h-full"
-						type="search"
-						placeholder="Select City"
-						onChange={handleChange}
-						onFocus={() => setDrop(true)}
-						onBlur={() => setDrop(false)}
-					/>
+				<div className="   w-[40vw]">
+					{drop ? (
+						<input
+							id="city-search"
+							className="shadow-none border-none bg-transparent focus:outline-none w-full h-full px-4 py-2"
+							type="search"
+							placeholder="Select City"
+							onChange={handleChange}
+							onBlur={() => unFocus()}
+						/>
+					) : (
+						<div
+							className={
+								"shadow-none border-none bg-transparent focus:outline-none w-full h-full text-left px-4 py-2" +
+								(html === "Select City" ? "text-gray-400" : "")
+							}
+							dangerouslySetInnerHTML={{ __html: html }}
+							onClick={() => setDrop(true)}
+						></div>
+					)}
 				</div>
-				<div className="bg-[#F3E59D] w-[40vw] px-4 py-2 ">
+				<div
+					className={
+						"bg-[#F3E59D] w-[40vw]   " + (drop ? "" : "hidden")
+					}
+				>
 					<ul
-						className=" rounded-lg overflow-hidden text-left"
+						className="rounded-lg overflow-hidden"
 						name="monster"
 						id="monsters"
 					>
 						{filteredMonsters.slice(0, 9).map((monster) => (
 							<li key={monster.city}>
-								<button onClick={listClick}>
+								<button
+									className="w-full h-full text-left px-4 py-2"
+									onClick={(e) => listClick(e)}
+								>
 									{monster.city}
 								</button>
 							</li>

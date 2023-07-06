5297bfc96f323e0d63f0003bfb6899fe4c03793f
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-07-07 00:54:04 +0530

    added env

--- src/firebase/firebaseApp.jsx
+++ src/firebase/firebaseApp.jsx
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+require('dotenv').config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDoFU_KkaPAuy4_YrdM0bfVEhNW3HtrevE",
-    authDomain: "flixdin-app.firebaseapp.com",
-    projectId: "flixdin-app",
-    databaseURL: "https://flixdin-app-default-rtdb.firebaseio.com/",
-    storageBucket: "flixdin-app.appspot.com",
-    messagingSenderId: "790969471633",
-    appId: "1:790969471633:web:d8bbc1a34fe78c13e0a530",
-    measurementId: "G-T7RWXYGY15"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    databaseURL: process.env.DATABASEURL,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase

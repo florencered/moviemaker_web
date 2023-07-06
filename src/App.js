16a4410328717d8dcd16ad96f91b1651d97661eb
Mahideep <mahipadala206@gmail.com>
2023-07-06 19:31:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Domainpage from "./components/home/sections/domain/Domainpage";
 import "./App.css";
 import Home from "./components/home/home";
+import Explore from "./components/explore/explore.jsx";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/domainpage" element={<Domainpage />} />
+          <Route path="/Explore" element={<Explore />} />
         </Routes>
       </BrowserRouter>
     </div>

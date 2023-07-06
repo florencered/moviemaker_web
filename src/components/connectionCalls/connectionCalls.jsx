e6d1af46ed21fdffbdb5c951c5488ebbe39b6764
Pranav S <pranav.s2020a@vitstudent.ac.in>
2023-07-07 01:05:37 +0530

    connectionCall updated with Buttons

--- src/components/connectionCalls/connectionCalls.jsx
+++ src/components/connectionCalls/connectionCalls.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+// need to add the SideBar and SearchBar will make a another PR once that is done.
+import React from "react";
+import Sections from "./HorizontalSection.jsx";
+import Card from "../home/sections/dist/dynamicCards.jsx";
+import SideBar from "../home/sidebar/sidebar.js";
 
-const connectionCalls = () => {
+function connectionCalls() {
   return (
-    <div>connectionCalls</div>
-  )
+    <div className="App flex flex-row shrink-1">
+      <div class="basis-1/5 bg-[#F7EAA9] rounded-r-3xl my-2">
+        <SideBar />
+      </div>{" "}
+      <div className="items-start pl-10 align-baseline">
+        <div className=" pl-10 pt-10 flex flex-col">
+          <div className="">
+            <h1 className="text-[#C6B34E] font-sans font-bold text-5xl w-full">
+              Connection Call{" "}
+            </h1>{" "}
+          </div>{" "}
+          <div className="flex flex-row pt-7 items-start float-left w-full">
+            <Sections />
+          </div>{" "}
+          <div className="pt-3 w-full">
+            <input
+              placeholder="Search"
+              type="text"
+              className="bg-[#C6B34E] text-black placeholder-black rounded-2xl w-full py-2 px-5"
+            />
+          </div>{" "}
+          <div className="pt-2 items-start flow-root ">
+            <Card />
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 }
-
-export default connectionCalls
+export default connectionCalls;
---
+++ src/components/home/sections/dist/slider.jsx
@@ -0,0 +1,65 @@
+import { Slider } from "@mui/material";
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+const marks = [
+  {
+    value: 0,
+    label: "0 Km",
+  },
+  {
+    value: 100,
+    label: "100 Km",
+  },
+];
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+export default function Radius() {
+  return (
+    <Box sx={{ width: 670, height: 139 }}>
+      <div className="bg-[#F7EAA9] p-10 rounded-xl shadow-slate-500 shadow-md">
+        <Slider
+          sx={{
+            "& .MuiSlider-thumb": {
+              color: "black",
+            },
+            "& .MuiSlider-track": {
+              color: "black",
+            },
+            "& .MuiSlider-rail": {
+              color: "#acc4e4",
+            },
+            "& .MuiSlider-active": {
+              color: "black",
+            },
+            "& .MuiSlider-valueLabel": {
+              fontSize: 12,
+              fontWeight: "bold",
+              top: -6,
+              backgroundColor: "unset",
+              color: "black",
+              "&:before": {
+                display: "none",
+              },
+              "& *": {
+                background: "transparent",
+                color: "black",
+              },
+            },
+          }}
+          size="small"
+          defaultValue={10}
+          getAriaValueText={valuetext}
+          marks={marks}
+          aria-label="Small"
+          valueLabelDisplay="auto"
+          min={0}
+          max={100}
+        />
+      </div>
+    </Box>
+  );
+}

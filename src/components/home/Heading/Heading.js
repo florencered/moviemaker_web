339e978e2c220be2afabd050b790f2ade2c27fab
Pranav S <pranav.s2020a@vitstudent.ac.in>
2023-07-07 18:27:50 +0530

    updated button to render only one button at a time

--- src/components/home/Heading/Heading.js
+++ src/components/home/Heading/Heading.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 function Heading() {
-  return (
-    <div>
-        <h1 class = "text-4xl font-bold text-[#C1AD42] flex ml-20 mt-4">Home</h1>
-    </div>
-  )
+    return (
+      <div>
+        <h1 class="text-5xl font-bold text-[#C1AD42] flex ml-32 mt-4">
+          {" "}
+          Home{" "}
+        </h1>{" "}
+      </div>
+    );
 }
 
 export default Heading

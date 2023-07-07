b11c4705eca8f7477f2e77d5d1a86042b0126b37
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-07-07 14:29:38 +0530

    minor issues

--- src/firebase/firebaseApp.jsx
+++ src/firebase/firebaseApp.jsx
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "flixdin-app.appspot.com",
-    authDomain: "flixdin-app.appspot.com",
-    projectId: "flixdin-app.appspot.com",
-    databaseURL: "flixdin-app.appspot.com",
+    apiKey: "AIzaSyDoFU_KkaPAuy4_YrdM0bfVEhNW3HtrevE",
+    authDomain: "flixdin-app.firebaseapp.com",
+    databaseURL: "https://flixdin-app-default-rtdb.firebaseio.com",
+    projectId: "flixdin-app",
     storageBucket: "flixdin-app.appspot.com",
-    messagingSenderId: "flixdin-app.appspot.com",
-    appId: "flixdin-app.appspot.com",
-    measurementId: "flixdin-app.appspot.com",
+    messagingSenderId: "790969471633",
+    appId: "1:790969471633:web:d8bbc1a34fe78c13e0a530",
+    measurementId: "G-T7RWXYGY15"
 };
 
 // Initialize Firebase

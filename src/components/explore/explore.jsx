76c19249597130f8c37d7a73276e0ee9b48e703b
Sri-Chakradhar <gsrichakradhar1@gmail.com>
2023-07-07 13:02:47 +0530

    Explore page

--- src/components/explore/explore.jsx
+++ src/components/explore/explore.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Exploreimage from "./Exploreimage";
 import Nav from "../home/sidebar/sidebar.js";
 
+
 const Explora = () => {
   return (
     <div className="flex">
       <div class="bg-[#F7EAA9] rounded-r-2xl mb-2 pb-3 w-1/5 h-fit">
-        <Nav src="/images/logo.png" />
+        <Nav props1="/images/logo.png"></Nav>
       </div>
       <div className="flex justify-center ml-3 pl-4">
         <div>
@@ -14,12 +15,6 @@ const Explora = () => {
             <h1 className=" shadow-black">
               <img src="/images/Explorelogo.png" alt="Heading" />
             </h1>
-          </div>
-          <div className="py-4 my-3 pb-1.5 flex">
-            <button className="genre">All</button>
-            <button className="genre">NearMe</button>
-            <button className="genre">Location</button>
-            <button className="genre">Domain</button>
           </div>
           <div>
             <div className="py-3 pr-4 text-[#5B4E07]">
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,39 +1,51 @@
 // import './App.css';
-import Nav from './sidebar/sidebar';
+import Nav from "./sidebar/sidebar";
 // import Flixlogo from "..../"
 // image: "/images/flix_Logo.svg"
-import Heading from './Heading/Heading';
-import Section from './sections/sections';
-import Post from './connectionCalls_home/connectionCalls_home'
-import PostCard from './post/post';
-import ProfileList from './cards/card2';
-import Profile from './cards/card1'
-import Carousel from './caro/Carousel';
+import Heading from "./Heading/Heading";
+import Post from "./connectionCalls_home/connectionCalls_home";
+// import PostCard from "./post/post";
+import ProfileList from "./cards/card2";
+import Profile from "./cards/card1";
+import Carousel from "./caro/Carousel";
 // import "./caro/carousel.css";
 // import "./caro/carousel_styling"
-import "./styles/carousel.css"
+import "./styles/carousel.css";
+import useButtonGroup from "./sections/domain/useButtonGroup";
+import RadioButtons from "./sections/domain/RadioButtons";
+import Sections from "./sections/sections";
+import PostCard from "./sections/domain/Domainpost";
 
-function home() {
+function Home() {
+  const [isOpen, isClicked, toggleDropdown] = useButtonGroup();
+
   return (
     <>
-    <div className="App flex flex-row">
-      <div class="basis-1/4 bg-[#F7EAA9] rounded-r-3xl my-2">
-        <Nav></Nav>
+      <div className="App flex flex-row">
+        <div class=" rounded-r-2xl mb-2 pb-3 w-1/5 h-fit bg-[#F7EAA9]">
+          <Nav></Nav>
+        </div>
+        <div class="h-max md:basis-1/2 pr-3 relative">
+          <Heading></Heading>
+          <Sections isClicked={isClicked} toggleDropdown={toggleDropdown} />
+
+          {isOpen && (
+            <div className="z-96 items-center justify-center display: block;">
+              <RadioButtons className="max-w-md" />
+            </div>
+          )}
+
+          <Carousel />
+          <Post />
+          <PostCard />
+        </div>
+        <div class="basis-1/4">
+          <Profile></Profile>
+          <ProfileList></ProfileList>
+        </div>
       </div>
-      <div class="basis-1/2 pr-3">
-        <Heading></Heading>
-        <Section></Section>
-        <Carousel />
-        <Post></Post>
-        <PostCard></PostCard>
-      </div>
-      <div class="basis-1/4">
-        <Profile></Profile>
-        <ProfileList></ProfileList>
-      </div>
-    </div>
     </>
   );
 }
 
-export default home;
+export default Home;

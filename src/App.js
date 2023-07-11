a576e00c4ac90e2c59f82a6993aa8c0b1996b432
Mahideep <mahipadala206@gmail.com>
2023-07-11 13:47:50 +0530

    updates on comment sec

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Domainpage from "./components/home/sections/domain/Domainpage";
 import "./App.css";
 import Home from "./components/home/home";
 import Explore from "./components/explore/explore.jsx";
-import ModalShare from "./components/home/sections/domain/ModalShare";
 
 function App() {
   return (
@@ -17,7 +15,6 @@ function App() {
           <Route path="/Explore" element={<Explore />} />
         </Routes>
       </BrowserRouter>
-      <ModalShare />
     </div>
   );
 }

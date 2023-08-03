59599cd27c5f79165aa3c86ff406670ae78262fd
Sri-Chakradhar <gsrichakradhar1@gmail.com>
2023-08-03 15:29:51 +0530

    flick half way throuhg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
+import './index.css';
 import Home from "./components/home/home";
 import Explore from "./components/explore/explore.jsx";
+import Flicks from "./components/flicks/flicks.jsx";
 
 
 function App() {
   return (
-    <div className="App bg-[#FFFADD]">
+    <div className="App bg-[#FFFADD] h-screen">
       <BrowserRouter>
         <Routes>
         <Route path = "/home" element = {<Home/>}/>
         <Route path = "/explore" element = {<Explore />} />
+        <Route path = "/flicks" element = {<Flicks />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/flicks/flicks.jsx
+++ src/components/flicks/flicks.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Profile from "./profile";
+import { FaArrowLeft } from "react-icons/fa";
+import { BiCamera } from "react-icons/bi";
 
 const flicks = () => {
   return (
-    <div>flicks</div>
-  )
-}
+    <div className="grid h-screen place-items-center">
+      <div className="flex rounded-lg bg-[#F1E294] w-[45rem]">
+        <div className="">
+          <div className="py-4 px-28">
+            <span className="flex items-center justify-between relative top-7 z-10 opacity-80 text-white cursor-pointer">
+              <span className="flex items-center pl-2">
+                <FaArrowLeft className="" />
+                <span className="pl-2 text-sm ">Flicks</span>
+              </span>
+              <BiCamera className="left-3 mr-2" />
+            </span>
+            <img
+              src="/images/flicksimg/flickimages.png"
+              className="shadow-2xl relative rounded-lg z-1 bg-[#F1E294]"
+              alt="flicks"
+            />
+            <div className="bg-slate-600"></div>
+          </div>
+        </div>
+        <div>
+          <Profile />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default flicks
+export default flicks;

446ad05d77d0d28d2f19791fc5d56f83d52a7299
Riya bansal <103059954+Ri-yaB@users.noreply.github.com>
2023-08-03 11:27:19 +0530

    added myprofile section

--- src/components/myProfile/MyProfile.js
+++ src/components/myProfile/MyProfile.js
@@ -1,9 +1,147 @@
-import React from 'react'
-
-const MyProfile = () => {
+import React from "react";
+export const MyProfile = () => {
   return (
-    <div>MyProfile</div>
-  )
-}
+    
+    <div className="l" class="bg-white flex flex-row justify-center w-full">
+      <div className="profilepage-l">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="group">
+              <div className="overlap-group-wrapper">
+                <div className="div">
+                  <div className="element-likes">
+                    918
+                    <br />
+                    <br />
+                    likes
+                  </div>
+                  <div className="element-following">
+                    64
+                    <br />
+                    <br />
+                    following
+                  </div>
+                  <div className="element-followers">
+                    882
+                    <br />
+                    <br />
+                    followers
+                  </div>
+                  <div className="element-posts">
+                    12
+                    <br />
+                    <br />
+                    posts
+                  </div>
+                </div>
+              </div>
+              <div className="overlap-2">
+                <h1 className="text-wrapper">samrzkhan</h1>
+                <img className="vector" alt="Vector" src="assets/vector-10.png" />
+              </div>
 
+              <div className="group-2" />
+            </div>
+            
+  
+            
+            <div className="overlap-wrapper">
+              <div className="div-wrapper">
+                <div className="text-wrapper-3">Edit Profile</div>
+                
+              </div>
+            </div> 
+            <div className="over-wrapper">
+                <div className="divi-wrapper">
+                    <div className="text-wrap">Share profile</div>
+                </div>
+            </div>
+            
+           <div className="group-3">
+              <div className="overlap-3">
+                <div className="text-wrapper-4">Other Skills</div>
+              </div>
+            </div>
+            <div className="group-4">
+                <div className="ov">
+            <div className="text-wrapper-5">Settings</div>
+            </div>
+            </div>
+            
+  </div> 
+          <div className="overlap-4">
+            <div className="group-5">
+              <div className="text-wrapper-6">Sameer Khan</div>
+              <div className="text-wrapper-7">Director</div>
+              <p className="p">I didn't go to film school, I went to films.</p>
+            </div>
+            <div className="box">
+      <div className="group-wrapper">
+        <div className="group">
+          <img className="vector" alt="Vector" src="assets/vector.png" />
+          <img className="img" alt="Vector" src="assets/vector2.png" />
+          <img className="vector-2" alt="Vector" src="assets/vector3.jpg" />
+          <img className="vector-3" alt="Vector" src="assets/vector4.png" />
+          <img className="material-symbols" alt="Material symbols" src="assets/vector5.png" />
+          <img className="material-symbols-2" alt="Material symbols" src="assets/vector6.png" />
+          <img className="line" alt="Line" src="line82.png" />
+        </div>
+      </div>
+    </div>
+            
+            <div className="group-6">
+              <div className="overlap-5">
+                <div className="rectangle-2" />
+                <div className="text-wrapper-8">Portfolio</div>
+                <div className="text-wrapper-9">Sameer Khan</div>
+                <div className="text-wrapper-10">Director</div>
+                <div className="text-wrapper-11">My Work</div>
+                <div className="group-7">
+                  <div className="group-8">
+                    <div className="overlap-group-2">
+                      <div className="text-wrapper-12">http://my1stshortfilmteaser</div>
+                    </div>
+                  </div>
+                  <div className="group-9">
+                    <div className="overlap-6">
+                      <div className="text-wrapper-12">http://my3rdshortfilmteaser</div>
+                    </div>
+                  </div>
+                  <p className="text-wrapper-13">This is the link to the second teaser:</p>
+                  <div className="group-10">
+                    <div className="overlap-7">
+                      <div className="text-wrapper-12">http://my2ndshortfilmteaser</div>
+                    </div>
+                  </div>
+                  <p className="text-wrapper-14">This is the link to the third teaser:</p>
+                  <p className="text-wrapper-15">This is the link to the first teaser:</p>
+                </div>
+                <div className="text-wrapper-16">My Filmography</div>
+                <div className="text-wrapper-17">My Certificates</div>
+                <div className="group-11">
+                  <div className="overlap-8">
+                    <div className="text-wrapper-12">IFP Participation Certificate.pdf</div>
+                  </div>
+                </div>
+                <div className="group-12">
+                  <div className="overlap-9">
+                    <div className="text-wrapper-12">DWFF Participation Certificate.pdf</div>
+                  </div>
+                </div>
+                
+                <p className="text-wrapper-19">
+                  I have directed three short films: Job Done, By Myself, Alex Sinclair. I've produced two, and
+                  performed as a secondary actor in one film.
+                </p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+  </div>
+  
+  
+  );
+};
 export default MyProfile

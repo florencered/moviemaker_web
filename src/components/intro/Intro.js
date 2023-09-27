f979760df6bf80a4d1704d51298f1676b8e2ae1b
Yashrd2002 <94217776+Yashrd2002@users.noreply.github.com>
2023-09-27 21:37:02 +0530

    intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -4,9 +4,9 @@ const Intro = () => {
   return (
     <div className="w-full h-screen flex justify-center">
       <div className="w-10/12 h-full flex justify-between">
-        <img src="/images/images/reel.png" alt=".." className="h-screen w-24" />
-        <img src="/images/images/flix_Logo.png" alt=".." className="h-screen" />
-        <img src="/images/images/reel.png" alt=".." className="h-screen w-24" />
+        <img src="/images/images/reel.png" alt=".." className="h-screen md:block hidden w-24" />
+        <img src="/images/images/flix_Logo.png" alt=".." className="md:h-screen h-5/6" />
+        <img src="/images/images/reel.png" alt=".." className="h-screen md:block hidden w-24" />
       </div>
     </div>
   );
